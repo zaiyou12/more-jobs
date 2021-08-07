455dc6a3b5e0deebca7a5bf61393cda66ab4675c
aaron.so <zaiyou12@gmail.com>
2021-08-07 19:46:25 +0900

    💄 Update logo and favicon

--- front-end/src/components/AppNavigation.vue
+++ front-end/src/components/AppNavigation.vue
@@ -1,12 +1,13 @@
 <template>
-  <nav class="sticky shadow top-0 z-40 lg:z-50 w-full bg-blue-500">
+  <nav class="sticky shadow top-0 z-40 lg:z-50 w-full bg-blue-100">
     <div class="flex flex-none w-full max-w-8xl mx-auto">
       <ul class="h-12 flex items-center overflow-x-auto overflow-y-hidden px-4 sm:px-6 lg:mx-6 lg:px-0 xl:mx-8 text-base text-blue-100  whitespace-nowrap">
         <li class="flex font-bold">
           <AppLink
             name="home"
           >
-            MoreJobs
+            <img :src="logo" class="w-10 h-10 lg:hidden"/>
+            <img :src="logoLarge" class="h-10 hidden lg:block" />
           </AppLink>
         </li>
         <!-- <li class="flex ml-6">도급</li>
@@ -17,3 +18,8 @@
     </div>
   </nav>
 </template>
+
+<script setup>
+import logo from '../assets/logo.png'
+import logoLarge from '../assets/logo-large.png'
+</script>
