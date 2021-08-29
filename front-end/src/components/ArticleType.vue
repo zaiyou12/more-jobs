ff9756b463e72fceeb35e7986bec9d81776c2b99
Aaron So <zaiyou12@gmail.com>
2021-08-29 13:54:34 +0900

    ✨ Add work_type label

---
+++ front-end/src/components/ArticleType.vue
@@ -0,0 +1,14 @@
+<template>
+  <span v-if="workType==='도급'" class="rounded-full bg-blue-400 px-2 py-0.5 text-white text-sm">
+    {{ workType }}
+  </span>
+  <span v-else-if="workType==='상주'" class="rounded-full bg-indigo-400 px-2 py-0.5 text-white text-sm">
+    {{ workType }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  workType: String;
+}>();
+</script>
--- front-end/src/components/ArticlesListArticlePreview.vue
+++ front-end/src/components/ArticlesListArticlePreview.vue
@@ -1,14 +1,27 @@
 <template>
   <AppLink
     name="article"
-    :params="{workType, index:index.toString(), date: article.created_time, title: article.title }"
+    :params="{
+      workType,
+      index: index.toString(),
+      date: article.created_time,
+      title: article.title,
+    }"
   >
-    <article class="w-full p-2 break-all truncate border-t border-gray-300">
-      <strong class="text-lg">{{ article.title }}</strong>
+    <article class="w-full p-2 break-all truncate border-t border-gray-300 flex-col">
+      <div class="flex">
+        <ArticleType :workType="article.work_type? article.work_type: ''"/>
+      </div>
+      <div class="flex">
+        <strong class="flex text-lg">
+          {{ article.title }}
+        </strong>
+      </div>
       <ul class="flex mt-1">
-        <li>{{ article.work_type }}</li>
-        <li class="ml-3" v-if="article.price">￦{{ (article.price/10000).toLocaleString() }}만</li>
-        <li class="ml-3" v-else>견적 상호협의</li>
+        <li v-if="article.price">
+          ￦{{ (article.price / 10000).toLocaleString() }}만
+        </li>
+        <li v-else>견적 상호협의</li>
         <li class="ml-3" v-if="article.term">{{ article.term }}일</li>
         <li class="ml-3" v-else>일정 상호협의</li>
         <li class="ml-3">{{ article.created_time }}</li>
@@ -20,9 +33,10 @@
 <script lang="ts" setup>
 import { getArticlesMeta } from "../composable/useArticles";
 import AppLink from "./AppLink.vue";
+import ArticleType from "./ArticleType.vue"
 const props = defineProps<{
-  article: Article
-  index: number
-}>()
+  article: Article;
+  index: number;
+}>();
 const { workType } = getArticlesMeta();
 </script>
